ef4b7787d43e856154288dbcf1d48d49744bf271
William Gan <hswg94@gmail.com>
2024-01-31 18:52:28 +0800

    changed URLs for projectsData

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -4,14 +4,14 @@ const projectsData = [
     desc:
       "RateAnything is a user-centric web application built using Node.js, Express.js, MongoDB, and Bootstrap. It offers a versatile platform where users can express their opinions, discover new experiences, and engage with a vibrant community of reviewers.",
     image: "./images/projects/rateanything.png",
-    url: "https://rateanything.onrender.com/items",
+    url: "https://rateanything.up.railway.app/items",
   },
   {
     title: "eCommerce Concept",
     desc:
     "This is an eCommerce proof of concept built with MongoDB, Express.js, React.js, Redux, and Node.js,. It features Authentication, Authorization, Product Search, Product Ratings/Reviews, Checkout/Payment Gateway, & Administration Features.",
     image: "./images/projects/ecommercepoc.png",
-    url: "https://william-ecommercepoc.onrender.com/",
+    url: "https://ecommercepoc.up.railway.app/",
   },
   {
     title: "MERN Authentication",
