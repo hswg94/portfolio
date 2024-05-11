42ecbe5605b71d0d745e685936946e890116e7e4
William Gan <hswg94@gmail.com>
2024-05-12 01:01:09 +0800

    Reworked integration of socialsdata by mapping

--- src/pages/ContactsSection.jsx
+++ src/pages/ContactsSection.jsx
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/icons/contact-img.svg";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
+import socialsData from "../data/socialsData";
 
 const ContactsSection = () => {
   const formInitialDetails = {
@@ -51,7 +52,8 @@ const ContactsSection = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} md={6}>
-            <TrackVisibility>
+            <TrackVisibility
+              once={true}>
               {({ isVisible }) => (
                 <img
                   className={
@@ -64,65 +66,14 @@ const ContactsSection = () => {
             </TrackVisibility>
           </Col>
           <Col size={12} md={6}>
-            <h2>Get In Touch</h2>
-            <form onSubmit={handleSubmit}>
-              <Row>
-                <Col size={12} sm={6} className="px-1">
-                  <input
-                    type="text"
-                    value={formDetails.firstName}
-                    placeholder="First Name"
-                    onChange={(e) => onFormUpdate("firstName", e.target.value)}
-                  />
-                </Col>
-                <Col size={12} sm={6} className="px-1">
-                  <input
-                    type="text"
-                    value={formDetails.lasttName}
-                    placeholder="Last Name"
-                    onChange={(e) => onFormUpdate("lastName", e.target.value)}
-                  />
-                </Col>
-                <Col size={12} sm={6} className="px-1">
-                  <input
-                    type="email"
-                    value={formDetails.email}
-                    placeholder="Email Address"
-                    onChange={(e) => onFormUpdate("email", e.target.value)}
-                  />
-                </Col>
-                <Col size={12} sm={6} className="px-1">
-                  <input
-                    type="tel"
-                    value={formDetails.phone}
-                    placeholder="Phone No."
-                    onChange={(e) => onFormUpdate("phone", e.target.value)}
-                  />
-                </Col>
-                <Col size={12} className="px-1">
-                  <textarea
-                    rows="6"
-                    value={formDetails.message}
-                    placeholder="Message"
-                    onChange={(e) => onFormUpdate("message", e.target.value)}
-                  ></textarea>
-                  <button type="submit">
-                    <span>{buttonText}</span>
-                  </button>
-                </Col>
-                {status.message && (
-                  <Col>
-                    <p
-                      className={
-                        status.success === false ? "danger" : "success"
-                      }
-                    >
-                      {status.message}
-                    </p>
-                  </Col>
-                )}
-              </Row>
-            </form>
+            <h2 className="my-3">Get In Touch</h2>
+            <div className="d-flex gap-4">
+              {socialsData.map(({icon: Icon, url}, index) => (
+              <a key={index} href={url} target="_blank" rel="noopener noreferrer">
+                <Icon size={50} color="white"/>
+              </a>
+            ))}
+            </div>
           </Col>
         </Row>
       </Container>
