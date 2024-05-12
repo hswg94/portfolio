ecf66c1d66d5734441df031c16e9a56ed88190ae
William Gan <hswg94@gmail.com>
2024-05-12 21:53:57 +0800

    cleaned up formatting

--- src/pages/ContactsSection.jsx
+++ src/pages/ContactsSection.jsx
@@ -6,66 +6,20 @@ import TrackVisibility from "react-on-screen";
 import socialsData from "../data/socialsData";
 
 const ContactsSection = () => {
-  const formInitialDetails = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    message: "",
-  };
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState("Send");
-  const [status, setStatus] = useState({});
-
-  const onFormUpdate = (category, value) => {
-    setFormDetails({
-      ...formDetails,
-      [category]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    setFormDetails(formInitialDetails);
-    if (result.code == 200) {
-      setStatus({ succes: true, message: "Message sent successfully" });
-    } else {
-      setStatus({
-        succes: false,
-        message: "Something went wrong, please try again later.",
-      });
-    }
-  };
-
   return (
     <section className="contact" id="connect">
       <Container>
         <Row className="align-items-center">
-          <Col size={12} md={6}>
-            <TrackVisibility
-              once={true}>
+          <Col sm={12} md={6}>
+            <TrackVisibility once>
               {({ isVisible }) => (
-                <img
-                  className={
-                    isVisible ? "animate__animated animate__zoomIn" : ""
-                  }
+                <img className={isVisible ? "animate__animated animate__zoomIn" : ""}
                   src={contactImg}
-                  alt="Contact Us"
                 />
               )}
             </TrackVisibility>
           </Col>
-          <Col size={12} md={6}>
+          <Col sm={12} md={6}>
             <h2 className="my-3">Get In Touch</h2>
             <div className="d-flex gap-4">
               {socialsData.map(({icon: Icon, url}, index) => (
