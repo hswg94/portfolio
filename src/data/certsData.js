0ba7858a51106f01cecc9bdb2e407db0a19de14a
William Gan <hswg94@gmail.com>
2024-05-12 15:36:34 +0800

    Added AWS Certificate

--- src/data/certsData.js
+++ src/data/certsData.js
@@ -1,6 +1,12 @@
 const certsData = [
   {
-    title: "eCommerce Concept with MERN",
+    title: "AWS Certified Cloud Practitioner",
+    date: "27th Mar 2024",
+    image: "./images/certs/CertifiedCloudPractitionerCertificate.png",
+    url: "https://cp.certmetrics.com/amazon/en/public/verify/credential/ab492459d42740f0bbf0e879b1b22e9b",
+  },
+  {
+    title: "eCommerce Platform Concepts",
     date: "23rd Oct 2023",
     image: "./images/certs/eCommerceConcept.png",
     url: "https://www.udemy.com/certificate/UC-e1bc31b2-2cde-426d-a47c-cb3582abcae6/",
@@ -12,9 +18,9 @@ const certsData = [
     url: "https://www.coursera.org/account/accomplishments/professional-cert/PCAZUNMQVJXP",
   },
   {
-    title: "Web Dev Bootcamp 2023",
+    title: "Web Developer Bootcamp 2023",
     date: "26th Apr 2023",
-    image: "./images/certs/WebDevBootcamp2023.png",
+    image: "https://udemy-certificate.s3.amazonaws.com/image/UC-01a21808-7e44-4703-a58d-36fd96b8268e.jpg",
     url: "https://www.udemy.com/certificate/UC-01a21808-7e44-4703-a58d-36fd96b8268e/",
   },
   {
@@ -22,12 +28,6 @@ const certsData = [
     date: "26th June 2023",
     image: "./images/certs/AdvancedReact.png",
     url: "https://www.coursera.org/account/accomplishments/verify/4XH675WAAR5B",
-  },
-  {
-    title: "Principles of UX/UI Design",
-    date: "2nd June 2023",
-    image: "./images/certs/PrinciplesUXUIDesign.png",
-    url: "https://www.coursera.org/account/accomplishments/verify/6S9URW8LWBDX",
   },
   {
     title: "Google Data Analytics",
