aa4765128339c10e58858f24a15476002b318e34
William Gan <hswg94@gmail.com>
2024-05-12 15:44:44 +0800

    Integrated cards to certificates section

--- src/pages/CertsSection.jsx
+++ src/pages/CertsSection.jsx
@@ -1,11 +1,6 @@
-import {
-  Container,
-  Carousel,
-  Row,
-  Col,
-  Image,
-} from "react-bootstrap";
+import { Container, Row, Col, Card, Image } from "react-bootstrap";
 import certsData from "../data/certsData";
+
 const CertsSection = () => {
   return (
     <section className="project">
@@ -14,20 +9,31 @@ const CertsSection = () => {
           <Col size={12}>
             <h1>Certificates</h1>
             <p>
-            These certifications represent my dedication to staying current in the ever-evolving landscape of technology and my passion for acquiring new skills.
+              These certifications represent my dedication to staying current in
+              the ever-evolving landscape of technology and my passion for
+              acquiring new skills.
             </p>
             <div className="d-flex justify-content-center">
-            <Carousel className="w-75 d-flex" interval={1000}>
-                {certsData.map((cert, index) => (
-                    <Carousel.Item key={index}>
-                        <a href={cert.url} target="_blank" rel="noreferrer">
-                          <Image src={cert.image}/>
-                        </a>
-                        <Carousel.Caption>
-                        </Carousel.Caption>
-                    </Carousel.Item>
+              <Row xs={1} md={3} className="g-3">
+                {certsData.map(({ title, date, image, url }, index) => (
+                  <Col key={index}>
+                    <a
+                      href={url}
+                      className="text-decoration-none"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Card href={url}>
+                        <Card.Img variant="top" src={image} />
+                        <Card.Body>
+                          <Card.Title>{title}</Card.Title>
+                          <Card.Text>{date}</Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </a>
+                  </Col>
                 ))}
-            </Carousel>
+              </Row>
             </div>
           </Col>
         </Row>
