4dca973af9e87f9d68da9c0a3da7417bd8ff2d4f
William Gan <hswg94@gmail.com>
2024-08-29 17:56:20 +0800

    updated vite config for instructions when pushing to AWS

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/"
+// if import to AWS set  to "/" to solve 403 forbidden
+  base: "/portfolio"
 })
