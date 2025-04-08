aaa7b1369880bfc4c21e14494f5053b59d7ec813
William Gan <hswg94@gmail.com>
2025-04-08 10:53:45 +0800

    updated project description and link

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,20 +1,20 @@
 const projectsData = [
   {
-    title: "RateAnything",
+    title: "eCommerce PoC",
+    desc: "This is an eCommerce proof of concept hosted on AWS and provisioned with Terraform. The application is built with MongoDB, Express.js, React.js, Redux, and Node.js. It features Authentication, Authorization, Product Search, Product Ratings/Reviews, Checkout/Payment Gateway, & Administration Features.",
+    images: [
+      "./images/projects/ecommercepoc.webp",
+    ],
+    url: "https://ecomm.hswg94.com/",
+  },
+  {
+    title: "RateAnything (Offline)",
     desc: "RateAnything is a user-centric web application built using Node.js, Express.js, MongoDB, and Bootstrap. It offers a versatile platform where users can express their opinions, discover new experiences, and engage with a vibrant community of reviewers.",
     images: [
       "./images/projects/rateanything.webp",
     ],
     url: "https://rateanything.up.railway.app/items",
-  },
-  {
-    title: "eCommerce",
-    desc: "This is an eCommerce proof of concept built with MongoDB, Express.js, React.js, Redux, and Node.js. It features Authentication, Authorization, Product Search, Product Ratings/Reviews, Checkout/Payment Gateway, & Administration Features.",
-    images: [
-      "./images/projects/ecommercepoc.webp",
-    ],
-    url: "https://ecommercepoc.up.railway.app/",
-  },
+  }
 ];
 
 export default projectsData;
